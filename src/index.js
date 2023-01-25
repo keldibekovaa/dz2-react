e263beb86da84496f21bc1510641e82c50aaa58b
keldibekovaa <115090119+keldibekovaa@users.noreply.github.com>
2023-01-25 14:42:17 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Timer from './timer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Timer/>
+  </React.StrictMode>
+);
+
---
+++ src/timer.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+function Timer() {
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+
+}
+
+export default Timer;
